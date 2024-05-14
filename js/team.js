9a622ff4b23f667119d1682b579a5361861bbfef
angelofaoldi <faoldiangelo@gmail.com>
2024-05-14 15:40:01 +0200

    [UPD-FIX] Milestone 0

--- js/team.js
+++ js/team.js
@@ -1,20 +1,27 @@
 'use strict';
 
 /******************************************************************************
-MILESTONE 1
+MILESTONE 0
 Creare array di oggetti con le informazioni fornite 
 *****************************************************************************/
 
 // creo sei oggetti, uno per ogni persona
-let p1 = {nome: "Wayne Barnett", role: "Founder & CEO"};
-let p2 = {nome: "Angela Caroll", role: "Chief editor"};
-let p3 = {nome: "Walter Gordon", role: "Office Manager"};
-let p4 = {nome: "Angela Lopex", role: "Social Media Manager"};
-let p5 = {nome: "Scott Estrada", role: "Developer & CEO"};
-let p6 = {nome: "Barbara Ramos", role: "Graphic Designer"};
+let p1 = {nome: "Wayne Barnett", role: "Founder & CEO", image: "wayne-barnett-founder-ceo.jpg"};
+let p2 = {nome: "Angela Caroll", role: "Chief editor", image: "angela-caroll-chief-editor.jpg"};
+let p3 = {nome: "Walter Gordon", role: "Office Manager", image: "walter-gordon-office-manager.jpg"};
+let p4 = {nome: "Angela Lopez", role: "Social Media Manager", image: "angela-lopez-social-media-manager.jpg"};
+let p5 = {nome: "Scott Estrada", role: "Developer & CEO", image: "scott-estrada-developer.jpg"};
+let p6 = {nome: "Barbara Ramos", role: "Graphic Designer", image: "barbara-ramos-graphic-designer.jpg"};
 
-console.log(p1);
+console.log(p1); console.log(p2); console.log(p3);
+console.log(p4); console.log(p5); console.log(p6);
 
 // pArray contiene tutti e sei gli oggetti
 const pArray = [p1, p2, p3, p4, p5, p6];
 console.log(pArray);
+
+/******************************************************************************
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+*****************************************************************************/
+
